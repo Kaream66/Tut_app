56147d69e08f7690b0645445b9203657a28cd863
Kaream <kareamtanany@gmail.com>
2026-09-10 12:00:51 +0300

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TutApp());
+}
+
+class TutApp extends StatelessWidget {
+  const TutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
