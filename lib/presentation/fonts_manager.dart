9e38baaeb1f274db139cf7e970fb709050352f43
Kaream <kareamtanany@gmail.com>
2026-09-10 14:19:38 +0300

    add font family and create fonts manager

---
+++ lib/presentation/fonts_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FontsManager {
+  static final String fontFamily = 'Montserrat';
+}
+
+class FontsWeight {
+  static final FontWeight light = FontWeight.w300;
+  static final FontWeight regular = FontWeight.w400;
+  static final FontWeight medium = FontWeight.w500;
+  static final FontWeight semiBold = FontWeight.w600;
+  static final FontWeight bold = FontWeight.w700;
+}
